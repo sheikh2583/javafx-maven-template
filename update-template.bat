486edb1ef159cf663d759af7c53de4b83537d53b
Sheikh Mosheul Akbar <sheikhakbar@iut-dhaka.edu>
2025-05-20 05:42:52 +0600

    Update project

--- update-template.bat
+++ update-template.bat
@@ -13,6 +13,9 @@ echo.
 echo Committing...
 git commit -m "%msg%"
 
+:: 📝 Append to changelog.txt
+echo [%date% %time%] %msg% >> changelog.txt
+
 echo.
 echo Pulling latest from origin...
 git pull --rebase origin main
@@ -23,4 +26,8 @@ git push
 
 echo.
 echo ✅ Project successfully pushed!
+
+:: 🌐 Open GitHub in default browser
+start https://github.com/sheikh2583/javafx-maven-template
+
 pause
