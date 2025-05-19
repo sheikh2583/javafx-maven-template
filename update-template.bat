41971cee70976543e7dcaa0e46fab72348994d2b
Sheikh Mosheul Akbar <sheikhakbar@iut-dhaka.edu>
2025-05-20 05:15:23 +0600

    ➕ Add auto-update script for template maintenance

---
+++ update-template.bat
@@ -0,0 +1,22 @@
+@echo off
+set /p msg="Enter commit message (leave blank for 'Update project'): "
+
+if "%msg%"=="" (
+    set msg=Update project
+)
+
+echo.
+echo Adding all files...
+git add .
+
+echo.
+echo Committing...
+git commit -m "%msg%"
+
+echo.
+echo Pushing to GitHub...
+git push
+
+echo.
+echo ✅ Project successfully pushed!
+pause
